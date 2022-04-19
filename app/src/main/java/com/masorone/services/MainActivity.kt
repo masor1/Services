d5e11ba57bc1aaa45e31d4695b3e846264e172aa
masor1 <masor.dev@gmail.com>
2022-04-19 21:00:46 +0300

    add viewBinding in MainActivity.kt

--- app/src/main/java/com/masorone/services/MainActivity.kt
+++ app/src/main/java/com/masorone/services/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.masorone.services
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.masorone.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
