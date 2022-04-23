29575c138e2e073197023296679bfdcb954acdea
masor1 <masor.dev@gmail.com>
2022-04-23 23:03:04 +0300

    job scheduler

--- app/src/main/java/com/masorone/services/MyJobService.kt
+++ app/src/main/java/com/masorone/services/MyJobService.kt
@@ -2,6 +2,9 @@ package com.masorone.services
 
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.content.Intent
+import android.os.Build
+import android.os.PersistableBundle
 import android.util.Log
 import kotlinx.coroutines.*
 
@@ -16,12 +19,23 @@ class MyJobService : JobService() {
 
     override fun onStartJob(params: JobParameters?): Boolean {
         log("onStartCommand")
-        coroutineScope.launch {
-            for (i in 0 until 30) {
-                delay(1000)
-                log("Timer $i")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            coroutineScope.launch {
+
+                var workItem = params?.dequeueWork()
+
+                while (workItem != null) {
+                    val page = workItem.intent.getIntExtra(PAGE, 0)
+                    for (i in 0 until 5) {
+                        delay(1000)
+                        log("Timer $i : $page")
+                    }
+                    params?.completeWork(workItem)
+                    workItem = params?.dequeueWork()
+                }
+                jobFinished(params, false)
             }
-            jobFinished(params, true)
         }
         return true
     }
@@ -42,6 +56,14 @@ class MyJobService : JobService() {
 
     companion object {
 
-        const val JOD_ID = 111
+        const val JOB_ID = 111
+
+        private const val PAGE = "page"
+
+        fun newIntent(page: Int): Intent {
+            return Intent().apply {
+                putExtra(PAGE, page)
+            }
+        }
     }
 }
